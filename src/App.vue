57d60faddbd1dccbbda8f1ae84146fe99bd93f71
Drazen Plavsic <drazen.p@live.com>
2025-03-27 11:49:08 +0100

    h3 implemented for month

--- src/App.vue
+++ src/App.vue
@@ -30,6 +30,7 @@ console.log(days(start));
 
 <template>
   <div v-for="month in months(start, end)">
+    <h3>{{ month.format("MMMM") }}</h3>
     <div v-for="day in days(month)">{{ day.format("YYYY-MM-DD") }}</div>
   </div>
 </template>
